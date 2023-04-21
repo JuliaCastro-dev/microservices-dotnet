41ed047e4c73c5441ed69df35df3f91e6e2b3a9d
JuliaCastro-dev <juliaprofessional21@gmail.com>
2023-04-21 10:11:04 -0300

    Feat: Create GeekShoppingWeb and GeekShopping.ProductAPI, added database and execute migrations

---
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(IConfiguration configuration) 
+        {
+            Configuration = configuration;
+        }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
+
+        protected readonly IConfiguration Configuration;
+
+ 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to mysql with connection string from app settings
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            
+            options.UseMySql(connection, ServerVersion.AutoDetect(connection));
+        }
+
+        public DbSet<Product> Products { get; set;}
+
+    }
+}
