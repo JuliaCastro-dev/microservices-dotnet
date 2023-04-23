69bc5d4817cc1ae6fa5c251da5e51993115ac5b9
JuliaCastro-dev <juliaprofessional21@gmail.com>
2023-04-23 18:37:58 -0300

    fix: update connection database .Net 6 LTS

--- GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -4,22 +4,8 @@ namespace GeekShopping.ProductAPI.Model.Context
 {
     public class MySQLContext : DbContext
     {
-        public MySQLContext(IConfiguration configuration) 
-        {
-            Configuration = configuration;
-        }
-        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
-
-        protected readonly IConfiguration Configuration;
-
- 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            // connect to mysql with connection string from app settings
-            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
-            
-            options.UseMySql(connection, ServerVersion.AutoDetect(connection));
-        }
+        public MySQLContext() { }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set;}
 
--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,19 +1,27 @@
 using AutoMapper;
 using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Model.Context;
 using GeekShopping.ProductAPI.Repository;
 using GeekShopping.ProductAPI.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connection = builder.Configuration["MySQlConnection:MySQlConnectionString"];
 
+// Add services to the container.
+builder.Services.AddDbContext<MySQLContext>(options => options.
+              UseMySql(connection,
+                      new MySqlServerVersion(
+                          new Version(8, 0, 5))));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
 });
 
 
